0263d6f32714f567387e9bb846ab0de18023d407
Gromova Maria <masia86@mail.ru>
2022-12-22 12:08:49 +0300

    добавлена проверка полей формы входа

--- js/initFormValid.js
+++ js/initFormValid.js
@@ -1,4 +1,4 @@
-//валидация полей формы
+//валидация полей формы в разделе "О нас"
 new JustValidate('.feedback', {
   colorWrong: '#D52B1E',
   rules: {
@@ -23,6 +23,27 @@ new JustValidate('.feedback', {
     email: {
       required: 'Вы не ввели e-mail',
       email: 'Вы ввели некорректный e-mail'
+    }
+  }
+});
+
+// валидация полей формы в разделе "вход"
+new JustValidate('.entrance__form', {
+  colorWrong: '#D52B1E',
+  rules: {
+    login: {
+      required: true,
+    },
+    password: {
+      required: true,
+    }
+  },
+  messages: {
+    login: {
+      required: 'Вы не ввели логин',
+    },
+    password: {
+      required: 'Вы не ввели пароль'
     }
   }
 });
